c720f1364f7a7839fc2dd692586fa88933613fb0
Stepan-Store777 <134931398+Stepan-Store777@users.noreply.github.com>
2024-03-01 11:42:50 -0300

    reset css e link

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function Sobre () {
+
+    return(
+      <>
+      <h1>Sobre</h1>
+      <p> Telas sobre meu projeto</p>
+      <Link href={"/"}> Home </Link>
+      </>
+    );
+} 
